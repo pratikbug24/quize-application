2438e60e261522e9510002b4383444bfd9cbd9f8
pratikbug24 <PP7701264@GMAIL.COM>
2024-05-18 11:10:44 +0530

    add files
    c programming quize

---
+++ app.js
@@ -0,0 +1,440 @@
+/* Created and coded by Abhilash Narayan */
+/* Quiz source: w3schools.com */
+var quiz = {
+"JS": [
+{
+"id": 1,
+"question": "Who is the father of C language?",
+"options": [
+{
+"a": "Steve Jobs;",
+"b": "James Gosling",
+"c": "Rasmus Lerdorf",
+"d": "Dennis Ritchie"
+}
+],
+"answer": "Dennis Ritchie",
+"score": 0,
+"status": ""
+},
+{
+"id": 2,
+"question": "Which of the following is not a valid C variable name?",
+"options": [
+{
+"a": "int number;",
+"b": "int variable_count;",
+"c": "int $main;",
+"d": " float rate;"
+}
+],
+"answer": "int $main;",
+"score": 0,
+"status": ""
+},
+{
+    "id": 3,
+    "question": "What is #include <stdio.h>?",
+    "options": [
+    {
+    "a": " Preprocessor directive",
+    "b": "File inclusion directive",
+    "c": "header file",
+    "d": "Non of those"
+    }
+    ],
+    "answer": "header file",
+    "score": 0,
+    "status": ""
+    },
+    {
+        "id": 4,
+        "question": "In C language, FILE is of which data type?",
+        "options": [
+        {
+        "a": " int",
+        "b": "char *",
+        "c": "struct",
+        "d": "None of the mentioned"
+        }
+        ],
+        "answer": "struct",
+        "score": 0,
+        "status": ""
+        },
+        {
+            "id": 5,
+            "question": "What is the sizeof(char) in a 32-bit C compiler?",
+            "options": [
+            {
+            "a": "1 bit",
+            "b": "2 bits",
+            "c": "1 Byte",
+            "d": "2 Bytes"
+            }
+            ],
+            "answer": "1 Byte",
+            "score": 0,
+            "status": ""
+            },
+            {
+                "id": 6,
+                "question": "Library functions are wrapped inside the_ _ _ _ _?",
+                "options": [
+                {
+                "a": "Header File",
+                "b": "Library Functions",
+                "c": "Pointers",
+                "d": "Compiler"
+                }
+                ],
+                "answer": "Header Files",
+                "score": 0,
+                "status": ""
+                },
+                {
+                    "id": 7,
+                    "question": "_ _ _ _ is a value range of signed long int?",
+                    "options": [
+                    {
+                    "a": "-32768 to +32767 ",
+                    "b": "-2147483648 to +2147483647",
+                    "c": "-128 to +127",
+                    "d": "1 to 65536"
+                    }
+                    ],
+                    "answer": "-2147483648 to +2147483647",
+                    "score": 0,
+                    "status": ""
+                    },
+                    {
+                        "id": 8,
+                        "question": "Size of pointer variable is?",
+                        "options": [
+                        {
+                        "a": "4 byte",
+                        "b": "2 byte",
+                        "c": "1 byte",
+                        "d": "4 bit"
+                        }
+                        ],
+                        "answer": "2 bytes",
+                        "score": 0,
+                        "status": ""
+                        },
+                        {
+                            "id": 9,
+                            "question": "ASCII code for 'a' is?",
+                            "options": [
+                            {
+                            "a": "65",
+                            "b": "99",
+                            "c": "67",
+                            "d": "97"
+                            }
+                            ],
+                            "answer": "97",
+                            "score": 0,
+                            "status": ""
+                            },
+                            {
+                                "id": 10,
+                                "question": "Which one is not reserve keyword in C Language?",
+                                "options": [
+                                {
+                                "a": "auto",
+                                "b": "main",
+                                "c": "case",
+                                "d": "register"
+                                }
+                                ],
+                                "answer": "main",
+                                "score": 0,
+                                "status": ""
+                                },
+                                {
+                                    "id": 11,
+                                    "question": "Prototype of function means?",
+                                    "options": [
+                                    {
+                                    "a": "Name of Function",
+                                    "b": "Output of Function",
+                                    "c": "Input of Function",
+                                    "d": "Declaration of Function"
+                                    }
+                                    ],
+                                    "answer": "Input of Function",
+                                    "score": 0,
+                                    "status": ""
+                                    },
+                                    {
+                                        "id": 12,
+                                        "question": "The Default Parameter Passing Parameter is called as",
+                                        "options": [
+                                        {
+                                        "a": "Call by Value",
+                                        "b": "Call by Address",
+                                        "c": "Call by Reference",
+                                        "d": "Call by Name"
+                                        }
+                                        ],
+                                        "answer": "Call by Reference",
+                                        "score": 0,
+                                        "status": ""
+                                        },
+                                        {
+                                            "id": 13,
+                                            "question": "Structure is the_ _ _",
+                                            "options": [
+                                            {
+                                            "a": "Collection of only same data type",
+                                            "b": "Collection of only different data type",
+                                            "c": "Collection of same or different data type",
+                                            "d": "All the above"
+                                            }
+                                            ],
+                                            "answer": "Collection of same or different data type",
+                                            "score": 0,
+                                            "status": ""
+                                            },
+                                            {
+                                                "id": 14,
+                                                "question": "Which of the following accessesa variable in structure b?",
+                                                "options": [
+                                                {
+                                                "a": "b->var; ",
+                                                "b": "b.var;",
+                                                "c": "b-var;",
+                                                "d": "b>var;"
+                                                }
+                                                ],
+                                                "answer": "b.var;",
+                                                "score": 0,
+                                                "status": ""
+                                                },
+                                                {
+                                                    "id": 15,
+                                                    "question": "Which of the following is a properly defined struct ?",
+                                                    "options": [
+                                                    {
+                                                    "a": "struct {int a;}",
+                                                    "b": "struct a_struct {int a;}",
+                                                    "c": "struct a_struct int a;",
+                                                    "d": "struct a_struct {int a;};"
+                                                    }
+                                                    ],
+                                                    "answer": "struct a_struct {int a;};",
+                                                    "score": 0,
+                                                    "status": ""
+                                                    },
+                                                    {
+                                                        "id": 16,
+                                                        "question": "union is_ _ _ _?",
+                                                        "options": [
+                                                        {
+                                                        "a": "Inbuilt datatype",
+                                                        "b": "Inbuilt constant",
+                                                        "c": "User defined data type",
+                                                        "d": "All of the above"
+                                                        }
+                                                        ],
+                                                        "answer": "User defined data type",
+                                                        "score": 0,
+                                                        "status": ""
+                                                        },
+                                                        {
+                                                            "id": 17,
+                                                            "question": "What is the size of double data type?",
+                                                            "options": [
+                                                            {
+                                                            "a": "6 bytes",
+                                                            "b": "8 bytes",
+                                                            "c": "1 bytes",
+                                                            "d": "1 bit",
+                                                            }
+                                                            ],
+                                                            "answer": "8 bytes",
+                                                            "score": 0,
+                                                            "status": ""
+                                                            },
+                                                            {
+                                                                "id": 18,
+                                                                "question": "There are _ _ _ _ Keywords in C?",
+                                                                "options": [
+                                                                {
+                                                                "a": "32",
+                                                                "b": "34",
+                                                                "c": "64",
+                                                                "d": "48"
+                                                                }
+                                                                ],
+                                                                "answer": "32",
+                                                                "score": 0,
+                                                                "status": ""
+                                                                },
+                                                                {
+                                                                    "id": 19,
+                                                                    "question": "Let a=5 & b=7 then a*=b is ?",
+                                                                    "options": [
+                                                                    {
+                                                                    "a": "b=35",
+                                                                    "b": "a=35",
+                                                                    "c": "a=7",
+                                                                    "d": "b=12"
+                                                                    }
+                                                                    ],
+                                                                    "answer": "a=35",
+                                                                    "score": 0,
+                                                                    "status": ""
+                                                                    },
+                                                                    {
+                                                                        "id": 20,
+                                                                        "question": "Let a=5 & b=7 then a%=b is",
+                                                                        "options": [
+                                                                        {
+                                                                        "a": "b=5",
+                                                                        "b": "a=2",
+                                                                        "c": "b=1",
+                                                                        "d": "a=5"
+                                                                        }
+                                                                        ],
+                                                                        "answer": "a=5",
+                                                                        "score": 0,
+                                                                        "status": ""
+                                                                        },
+               
+]
+}
+var quizApp = function () {
+this.score = 0;
+this.qno = 1;
+this.currentque = 0;
+var totalque = quiz.JS.length;
+this.displayQuiz = function (cque) {
+this.currentque = cque;
+if (this.currentque >= totalque) {
+    // Show submit button
+    $('#next').hide();
+    $('#submit').show();
+    // Rest of the code to display result...
+}
+if (this.currentque < totalque) {
+$("#tque").html(totalque);
+$("#previous").attr("disabled", false);
+$("#next").attr("disabled", false);
+$("#qid").html(quiz.JS[this.currentque].id + '.');
+$("#question").html(quiz.JS[this.currentque].question);
+$("#question-options").html("");
+for (var key in quiz.JS[this.currentque].options[0]) {
+if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+$("#question-options").append(
+"<div class='form-check option-block'>" +
+"<label class='form-check-label'>" +
+"<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+quiz.JS[this.currentque].options[0][key] +
+"</span></label>"
+);
+}
+}
+}
+if (this.currentque <= 0) {
+$("#previous").attr("disabled", true);
+}
+if (this.currentque >= totalque) {
+$('#next').attr('disabled', true);
+for (var i = 0; i < totalque; i++) {
+this.score = this.score + quiz.JS[i].score;
+}
+return this.showResult(this.score);
+}
+}
+this.showResult = function (scr) {
+$("#result").addClass('result');
+$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+for (var j = 0; j < totalque; j++) {
+var res;
+
+
+}
+}
+this.checkAnswer = function (option) {
+var answer = quiz.JS[this.currentque].answer;
+option = option.replace(/</g, "&lt;") //for <
+option = option.replace(/>/g, "&gt;") //for >
+option = option.replace(/"/g, "&quot;")
+if (option == quiz.JS[this.currentque].answer) {
+if (quiz.JS[this.currentque].score == "") {
+quiz.JS[this.currentque].score = 1;
+quiz.JS[this.currentque].status = "correct";
+}
+} else {
+quiz.JS[this.currentque].status = "wrong";
+}
+}
+this.changeQuestion = function (cque) {
+this.currentque = this.currentque + cque;
+this.displayQuiz(this.currentque);
+}
+}
+var jsq = new quizApp();
+var selectedopt;
+$(document).ready(function () {
+jsq.displayQuiz(0);
+$('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+//var radio = $(this).find('input:radio');
+$(this).prop("checked", true);
+selectedopt = $(this).val();
+});
+});
+$('#next').click(function (e) {
+e.preventDefault();
+if (selectedopt) {
+jsq.checkAnswer(selectedopt);
+}
+jsq.changeQuestion(1);
+});
+$('#previous').click(function (e) {
+e.preventDefault();
+if (selectedopt) {
+jsq.checkAnswer(selectedopt);
+}
+jsq.changeQuestion(-1);
+});
+
+var totalTimeInSeconds = 15 * 60;
+
+// Function to update the timer
+function updateTimer() {
+    var minutes = Math.floor(totalTimeInSeconds / 60);
+    var seconds = totalTimeInSeconds % 60;
+    document.getElementById('timer').innerHTML = 'Time Remaining: ' + minutes + 'm ' + seconds + 's';
+    totalTimeInSeconds--;
+    
+    // Check if time is up
+    if (totalTimeInSeconds < 0) {
+        clearInterval(timerInterval); // Stop the timer
+        submitQuiz(); // Automatically submit the quiz
+    }
+}
+
+// Start the timer
+var timerInterval = setInterval(updateTimer, 1000);
+
+// Function to submit the quiz
+function submitQuiz() {
+    // You can replace this with your code to submit the quiz automatically
+    console.log('Quiz submitted automatically');
+}
+
+// Show submit button at the end of the quiz
+if (this.currentque = totalque) {
+    // Show submit button
+    $('#next').hide();
+    $('#submit').show();
+    // Rest of the code to display result...
+}
+
+$('#submit').click(function(e) {
+    e.preventDefault();
+    submitQuiz();
+});
